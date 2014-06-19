612dbda4ce64067e8dbd216f481536bda8582a5f
redglory <pedropinto.inbox@gmail.com>
2014-06-19 12:47:29 +0100

    changed configs to __init__.py

--- __init__.py
+++ __init__.py
@@ -2,3 +2,36 @@ from .main import EmberMediaManager
 
 def autoload():
 	return EmberMediaManager()
+
+
+config = [{
+    'name': 'embermediamanager',
+    'groups': [
+        {
+            'tab': 'notifications',
+            'list': 'notification_providers',
+            'name': 'embermediamanager',
+            'label': 'Ember Media Manager',
+            'description': 'Ember Media Manager 1.3.20+ versions',
+            'options': [
+                {
+                    'name': 'enabled',
+                    'default': 0,
+                    'type': 'enabler',
+                },
+                {
+                    'name': 'app_path',
+                    'label': 'Application Path',
+                    'default': 'C:\\Ember Media Manager BETA\\Ember Media Manager.exe',
+                },
+                {
+                    'name': 'app_args',
+                    'label': 'Custom Command Line Arguments',
+                    'advanced': True,
+                    'default': '-newauto -all -nowindow',
+                    'description': 'Choose your custom command line arguments to call Ember Media Manager, separated by <space>',
+                },
+            ],
+        }
+    ],
+}]        
--- main.py
+++ main.py
@@ -88,35 +88,3 @@ class EmberMediaManager(Plugin):
             log.error("Failed to call Ember Media Manager: %s", (traceback.format_exc()))
 
         return False
-
-config = [{
-    'name': 'embermediamanager',
-    'groups': [
-        {
-            'tab': 'notifications',
-            'list': 'notification_providers',
-            'name': 'embermediamanager',
-            'label': 'Ember Media Manager',
-            'description': 'Ember Media Manager 1.3.20+ versions',
-            'options': [
-                {
-                    'name': 'enabled',
-                    'default': 0,
-                    'type': 'enabler',
-                },
-                {
-                    'name': 'app_path',
-                    'label': 'Application Path',
-                    'default': 'C:\\Ember Media Manager BETA\\Ember Media Manager.exe',
-                },
-                {
-                    'name': 'app_args',
-                    'label': 'Custom Command Line Arguments',
-                    'advanced': True,
-                    'default': '-newauto -all -nowindow',
-                    'description': 'Choose your custom command line arguments to call Ember Media Manager, separated by <space>',
-                },
-            ],
-        }
-    ],
-}]        
